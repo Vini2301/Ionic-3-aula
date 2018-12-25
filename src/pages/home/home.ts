801cf42d8ad0a9120b5fe4b0da763a37cc00ddb2
Vinicius Arlei <viniciusarlei@live.com>
2018-12-25 14:54:47 -0200

    Adds feed page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FeedPage } from '../feed/feed';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
 
   }
 
+  goToFeedPage() {
+    this.navCtrl.push(FeedPage)
+  }
 }
