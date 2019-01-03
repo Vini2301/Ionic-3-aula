cc485c10913bee4c50473e268abec998db977e8a
Vinicius Arlei <viniciusarlei@live.com>
2019-01-03 14:38:36 -0200

    Adds function and parameters

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -6,11 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public nome_usuario:string = "Vinicius Arlei do Codigo";
+
   constructor(public navCtrl: NavController) {
   }
 
+  public somaDoisnumeros(num1:number, num2:number):void {
+    alert(num1+num2);
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    this.somaDoisnumeros(10, 99);
   }
 
 }
